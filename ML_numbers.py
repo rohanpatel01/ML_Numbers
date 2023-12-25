a3d4aad659edfa3e9b42cd6e75d89ca90c075a0a
rohanpatel01 <rohanppatel01@gmail.com>
2023-12-25 16:16:26 -0600

    modified structure to use numpy array to hold weights, biases, and neuron values for easier time computing

--- ML_numbers.py
+++ ML_numbers.py
@@ -8,12 +8,52 @@ import random
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
+def getRandomVal01():
+    return round(random.uniform(0, 1), 2)
+
+def seeNeuralNetworkVals():
+    ## see neurons
+    # print("inputLayer: ", inputLayer)
+    # print()
+    print("hiddenLayer1: ", hiddenLayer1)
+    print()
+    print("hiddenLayer2: ", hiddenLayer2)
+    print()
+    print("outputLayer: ", outputLayer)
+    print()
+
+
+    ## see weights and biases
+    print("weightsInputLayer: ", weightsInputLayer)
+    print()
+    print("weightsHiddenLayer1: ", weightsHiddenLayer1)
+    print()
+    print("weightsHiddenLayer2: ", weightsHiddenLayer2)
+    print()
+    print("biasVector: ", biasVector)
+    print()
+
+
+    ## see other data
+    lenH1 = len(hiddenLayer1)
+    lenH2 = len(hiddenLayer2)
+    lenOut = len(outputLayer)
+    lenBias = len(biasVector)
+
+    print("lenH1: ", lenH1)
+    print("lenH2: ", lenH2)
+    print("lenOut: ", lenOut)
+    print("lenBias: ", lenBias)
+
+
 
 mndata = MNIST('samples')
 images, labels = mndata.load_training()
+numLayersNN = 4 # input, Hidden 1, Hidden 2, Output
 
 index = 0
 currentImage = images[index]
+currentImageLabel = labels[index]
 
 inputLayerLen = len(currentImage)
 hiddenLayer1Len = 16
@@ -24,32 +64,31 @@ outputLayerLen = 10
 for x in range(inputLayerLen):
     currentImage[x] = currentImage[x] / 255
 
-# holds neuron values for each layer
-inputLayer = currentImage
-hiddenLayer1 = [0] * hiddenLayer1Len
-hiddenLayer2 = [0] * hiddenLayer2Len
-outputLayer = [0] * outputLayerLen
 
-numWeights = (len(inputLayer) * hiddenLayer1Len) + (hiddenLayer1Len * hiddenLayer2Len) + (hiddenLayer2Len * outputLayerLen)
+# neurons at each layer
+inputLayer = np.array(currentImage)
+hiddenLayer1 = np.array([getRandomVal01() for x in range(hiddenLayer1Len)])
+hiddenLayer2 = np.array([getRandomVal01() for x in range(hiddenLayer2Len)])
+outputLayer = np.array([getRandomVal01() for x in range(outputLayerLen)])
+
+# weights of connections between each layer
+weightsInputLayer = np.array([[getRandomVal01() for col in range(inputLayerLen)] for row in range(hiddenLayer1Len)])
+weightsHiddenLayer1 = np.array([[getRandomVal01() for col in range(hiddenLayer1Len)] for row in range(hiddenLayer2Len)])
+weightsHiddenLayer2 = np.array([[getRandomVal01() for col in range(hiddenLayer2Len)] for row in range(outputLayerLen)])
+
+# doesn't need to be np array itself b/c we will just be using this to hold the np arrays of the weights
+weightVector = [weightsInputLayer, weightsHiddenLayer1, weightsHiddenLayer2]
+
 numBiases = hiddenLayer1Len + hiddenLayer2Len + outputLayerLen
 
-# holds weights and bias values 
-weightVector = [0] * numWeights
-biasVector = [0] * numBiases
+biasVector = np.array([getRandomVal01() for x in range(numBiases)])
 
-# initialize weights and biases with random values for training 
-for i in range(numWeights):
-    weightVector[i] = round(random.uniform(0, 1), 2)
-
-for i in range(numBiases):
-    biasVector[i] = round(random.uniform(0, 1), 2)
-
-print("Initialized Structure")
+print("Initialized Structure with random values")
 
 
 def computeOutput():
     print("Compute Output")
-    
+
 
 
 
