160bb3f5bd3902e1c6d9e3270990cc4a9c7a0cc7
rohanpatel01 <rohanppatel01@gmail.com>
2023-12-24 22:20:18 -0600

    ensured got image and label correctly

--- ML_numbers.py
+++ ML_numbers.py
@@ -1,7 +1,15 @@
 import numpy as np
 import pickle
+import os.path
+import mnist
+from mnist import MNIST
+
+mndata = MNIST('samples')
+images, labels = mndata.load_training()
+
+imageIndex = 0
 
 
-print("hello")
+
 
 
--- process_image.py
+++ process_image.py
@@ -1,17 +1,52 @@
 import os.path
 import gzip
 import mnist
-
 from mnist import MNIST
 
 mndata = MNIST('samples')
-
 images, labels = mndata.load_training()
 
-print(images[0])
+imageIndex = 11
+
+def changeArray():
+    myPrint = images[index]
+
+    for x in range(len(myPrint)):
+        val = myPrint[x]
+        if val != 0:
+            myPrint[x] = '@'
+        else:
+            myPrint[x] = '.'
+
+
+def myPrint():
+    show = images[index]
+
+    i = 0
+    while (i < len(show)):
+        for x in range(28):
+            print(show[i + x], end="")
+        # print(show[i: i + 28])
+        print()
+        i += 28
+
+
+# print(images[0])
 # or
 # images, labels = mndata.load_testing()
 
-# print(mndata.display(images[0]))
+def main():
+    print(mndata.display(images[index]))
+    print(labels[index])
 
-print("work")
+    changeArray()
+    # myPrint()
+    print(images[index])
+
+
+    print("work")
+
+
+
+
+main()
