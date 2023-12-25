0c04cdfcf421f3652a7b102a14cee824f667d45d
rohanpatel01 <rohanppatel01@gmail.com>
2023-12-25 00:16:59 -0600

    starting computeOutput function

--- ML_numbers.py
+++ ML_numbers.py
@@ -5,53 +5,60 @@ import mnist
 from mnist import MNIST
 import random
 
-def initStructure():
-    mndata = MNIST('samples')
-    images, labels = mndata.load_training()
-
-    index = 0
-    currentImage = images[index]
-
-    inputLayerLen = len(currentImage)
-    hiddenLayer1Len = 16
-    hiddenLayer2Len = 16
-    outputLayerLen = 10
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
 
 
-    # change data values from mnist (0-255) to our (0-1)
-    for x in range(inputLayerLen):
-        currentImage[x] = currentImage[x] / 255
+mndata = MNIST('samples')
+images, labels = mndata.load_training()
 
-    # holds neuron values for each layer
-    inputLayer = currentImage
-    hiddenLayer1 = [0] * hiddenLayer1Len
-    hiddenLayer2 = [0] * hiddenLayer2Len
-    outputLayer = [0] * outputLayerLen
+index = 0
+currentImage = images[index]
 
-    numWeights = (len(inputLayer) * hiddenLayer1Len) + (hiddenLayer1Len * hiddenLayer2Len) + (hiddenLayer2Len * outputLayerLen)
-    numBiases = hiddenLayer1Len + hiddenLayer2Len + outputLayerLen
+inputLayerLen = len(currentImage)
+hiddenLayer1Len = 16
+hiddenLayer2Len = 16
+outputLayerLen = 10
 
-    # holds weights and bias values 
-    weightVector = [0] * numWeights
-    biasVector = [0] * numBiases
+# change data values from mnist (0-255) to our (0-1)
+for x in range(inputLayerLen):
+    currentImage[x] = currentImage[x] / 255
 
-    # initialize weights and biases with random values for training 
-    for i in range(numWeights):
-        weightVector[i] = round(random.uniform(0, 1), 2)
+# holds neuron values for each layer
+inputLayer = currentImage
+hiddenLayer1 = [0] * hiddenLayer1Len
+hiddenLayer2 = [0] * hiddenLayer2Len
+outputLayer = [0] * outputLayerLen
 
-    for i in range(numBiases):
-        biasVector[i] = round(random.uniform(0, 1), 2)
+numWeights = (len(inputLayer) * hiddenLayer1Len) + (hiddenLayer1Len * hiddenLayer2Len) + (hiddenLayer2Len * outputLayerLen)
+numBiases = hiddenLayer1Len + hiddenLayer2Len + outputLayerLen
+
+# holds weights and bias values 
+weightVector = [0] * numWeights
+biasVector = [0] * numBiases
+
+# initialize weights and biases with random values for training 
+for i in range(numWeights):
+    weightVector[i] = round(random.uniform(0, 1), 2)
+
+for i in range(numBiases):
+    biasVector[i] = round(random.uniform(0, 1), 2)
+
+print("Initialized Structure")
+
+
+def computeOutput():
+    print("Compute Output")
+    
 
 
 
 def main():
-    initStructure()
-
-
-
-
-
+    computeOutput()
 
 main()
+
+
+
 
 
