4e22b405f2bd562f8539b67ae302d596f7356e98
rohanpatel01 <rohanppatel01@gmail.com>
2023-12-25 22:11:51 -0600

    created Layers Class to allow for objects

--- ML_numbers.py
+++ ML_numbers.py
@@ -3,8 +3,10 @@ import pickle
 import os.path
 import mnist
 from mnist import MNIST
+from typing import Type
 import random
 import math
+import copy
 
 
 def sigmoid(x):
@@ -14,38 +16,61 @@ def getRandomVal01():
     return round(random.uniform(0, 1), 2)
 
 def seeNeuralNetworkVals():
+
+    for layer in layers:
+        print("Neurons")
+        print("Num Neurons: ", layer.numNeurons)
+        print(layer.neurons)
+        print()
+
+        print("Weights")
+        print(layer.weights)
+        print()
+
+        print("Biases")
+        print(layer.biases)
+        print()
+
+        print("------------------------------------------------------------")
+
+
     ## see neurons
-    # print("inputLayer: ", inputLayer)
+    # print("Neurons")
+    # print("inputLayer: ", inputLayer.neurons)
     # print()
-    print("hiddenLayer1: ", hiddenLayer1)
-    print()
-    print("hiddenLayer2: ", hiddenLayer2)
-    print()
-    print("outputLayer: ", outputLayer)
-    print()
+    # print("hiddenLayer1: ", hiddenLayer1.numNeurons)
+    # print()
+    # print("hiddenLayer2: ", hiddenLayer2.numNeurons)
+    # print()
+    # print("outputLayer: ", outputLayer.numNeurons)
+    # print()
 
 
-    ## see weights and biases
-    print("weightsInputLayer: ", weightsInputLayer)
-    print()
-    print("weightsHiddenLayer1: ", weightsHiddenLayer1)
-    print()
-    print("weightsHiddenLayer2: ", weightsHiddenLayer2)
-    print()
-    print("biasVector: ", biasVector)
-    print()
+    # ## see weights and biases
+    # print("weightsInputLayer: ", weightsInputLayer)
+    # print()
+    # print("weightsHiddenLayer1: ", weightsHiddenLayer1)
+    # print()
+    # print("weightsHiddenLayer2: ", weightsHiddenLayer2)
+    # print()
+    # print("biasVector: ", biasVector)
+    # print()
 
 
-    ## see other data
-    lenH1 = len(hiddenLayer1)
-    lenH2 = len(hiddenLayer2)
-    lenOut = len(outputLayer)
-    lenBias = len(biasVector)
+    # ## see other data
+    # lenInput = len(inputLayer)
+    # lenH1 = len(hiddenLayer1)
+    # lenH2 = len(hiddenLayer2)
+    # lenOut = len(outputLayer)
+    # lenBias = len(biasVector)
 
-    print("lenH1: ", lenH1)
-    print("lenH2: ", lenH2)
-    print("lenOut: ", lenOut)
-    print("lenBias: ", lenBias)
+    # print("lenInput: ", lenInput)
+    # print("lenH1: ", lenH1)
+    # print("lenH2: ", lenH2)
+    # print("lenOut: ", lenOut)
+    # print("lenBias: ", lenBias)
+
+
 
 
 def normalizedXavierWeightInit(numNodesPrevLayer, numNodesCurrentLayer):
@@ -58,48 +83,98 @@ images, labels = mndata.load_training()
 numLayersNN = 4 # input, Hidden 1, Hidden 2, Output
 
 index = 0
-currentImage = images[index]
-currentImageLabel = labels[index]
+# currentImage = images[index]
+# currentImageLabel = labels[index]
 
-inputLayerLen = len(currentImage)
-hiddenLayer1Len = 16
-hiddenLayer2Len = 16
-outputLayerLen = 10
+
+class Layer:
+
+    def __init__(self, previousLayer: Type['Layer'], numNeurons, neurons = None) -> None:
+        self.previousLayer = previousLayer
+        self.numNeurons = numNeurons
+
+        # done to allow for input layer to manually write neuron values
+        if neurons is not None:
+            self.neurons = neurons
+        else:
+            self.neurons = np.array([0 for x in range(numNeurons)])
+
+        if not previousLayer:
+            self.weights = None
+        else:
+            self.weights = np.array([[normalizedXavierWeightInit(previousLayer.numNeurons, numNeurons) for col in range(previousLayer.numNeurons)] for row in range(numNeurons)])
+        
+        self.biases = np.array([0 for x in range(numNeurons)])
+
+
+
+inputLayer = Layer(previousLayer=None, numNeurons=3, neurons=np.array([0.3, 0.5, 0.7]))
+
+inputLayerLen = inputLayer.numNeurons        # was len(currentImage)
+hiddenLayer1Len = 2      # was 16
+hiddenLayer2Len = 2    # was 16
+outputLayerLen = 2     # was 10
 
 # change data values from mnist (0-255) to our (0-1)
-for x in range(inputLayerLen):
-    currentImage[x] = currentImage[x] / 255
+# for x in range(inputLayerLen):
+#     currentImage[x] = currentImage[x] / 255
 
 
-# neurons at each layer
-inputLayer = np.array(currentImage)
-hiddenLayer1 = np.array([getRandomVal01() for x in range(hiddenLayer1Len)])
-hiddenLayer2 = np.array([getRandomVal01() for x in range(hiddenLayer2Len)])
-outputLayer = np.array([getRandomVal01() for x in range(outputLayerLen)])
+# neurons at each layer - init vals remain 0 b/c their av is calculated not set to random
+# inputLayer = np.array(currentImage)
+hiddenLayer1 = Layer(previousLayer=inputLayer, numNeurons=hiddenLayer1Len)
+hiddenLayer2 = Layer(previousLayer=hiddenLayer1, numNeurons=hiddenLayer2Len)
+outputLayer = Layer(previousLayer=hiddenLayer2, numNeurons=outputLayerLen)
 
-# weights of connections between each layer
-weightsInputLayer = np.array([[normalizedXavierWeightInit(inputLayerLen, hiddenLayer1Len) for col in range(inputLayerLen)] for row in range(hiddenLayer1Len)])
-weightsHiddenLayer1 = np.array([[normalizedXavierWeightInit(hiddenLayer1Len, hiddenLayer2Len) for col in range(hiddenLayer1Len)] for row in range(hiddenLayer2Len)])
-weightsHiddenLayer2 = np.array([[normalizedXavierWeightInit(hiddenLayer2Len, outputLayerLen) for col in range(hiddenLayer2Len)] for row in range(outputLayerLen)])
+layers = [inputLayer, hiddenLayer1, hiddenLayer2, outputLayer]
 
-# doesn't need to be np array itself b/c we will just be using this to hold the np arrays of the weights
-weightVector = [weightsInputLayer, weightsHiddenLayer1, weightsHiddenLayer2]
+inputLayer.weights = None
+hiddenLayer1.weights = np.array([[normalizedXavierWeightInit(inputLayer.numNeurons, hiddenLayer1.numNeurons) for col in range(inputLayer.numNeurons)] for row in range(hiddenLayer1.numNeurons)])
+hiddenLayer2.weights = np.array([[normalizedXavierWeightInit(hiddenLayer1.numNeurons, hiddenLayer2.numNeurons) for col in range(hiddenLayer1.numNeurons)] for row in range(hiddenLayer2.numNeurons)])
+outputLayer.weights = np.array([[normalizedXavierWeightInit(hiddenLayer2.numNeurons, outputLayer.numNeurons) for col in range(hiddenLayer2.numNeurons)] for row in range(outputLayer.numNeurons)])
 
-numBiases = hiddenLayer1Len + hiddenLayer2Len + outputLayerLen
 
-# bias values can be initialized to 0 b/c asymmetry breaking is provided by the small random numbers in the weights
-biasVector = np.array([0 for x in range(numBiases)])
-
+print("Before Compute")
 seeNeuralNetworkVals()
+
 
 
 def computeOutput():
     
     # just work on computing one layer
     # start with simpler system to make sure works then do with entire system
-    print("compute output")
+    # do so iteratively for each layer of the NN
+
+    print("Compute Output")
+
+    
+    # for i in range(numLayersNN - 1): # -1 b/c computing for new layers, excluding input layer
+    #     # print("weightVector[i]: ", weightVector[i])
+    #     # print("neuronVector[i]: ", neuronVector[i])
+
+    #     neuronVector[i + 1] = np.dot(weightVector[i], neuronVector[i])
+    #     print("dot product: ", np.dot(weightVector[i], neuronVector[i]))
+    #     # print("computeVector[i]: ", computeVector[i])
+    #     # print()
+
+    # print()
+    # print()
+    # print("------------------------------------------------------------")
+    # print("After")
+
+    # print("hiddenLayer1: ", hiddenLayer1)
+    # print()
+    # print("hiddenLayer2: ", hiddenLayer2)
+    # print()
+    # print("outputLayer: ", outputLayer)
+    # print()
 
 
+    # print("After Compute")
+    # seeNeuralNetworkVals()
+
+
+    
 def main():
     computeOutput()
 
